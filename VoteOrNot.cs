50d6dace589222f9c064897d3117e6c6a542c8e5
Raphael Rodrigues Silva <69489880+RapGod91@users.noreply.github.com>
2022-08-28 17:09:09 -0300

    Add files via upload

---
+++ VoteOrNot.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    { 
+	// Alteração realizada por Raphael
+        string ano;
+        ano = Console.ReadLine();
+        int anoInt = Convert.ToInt16(ano);
+        if(anoInt > 15 && < 18) {
+            Console.WriteLine("Pode votar");
+        
+        } else if (anoInt >=18 ){   
+            Console.WriteLine("Tem que voltar")
+        }
+            else {
+            Console.WriteLine("Nao Pode votar");
+        }
+    
+    }
+
+}
