34dc81e69a1dd33a41d9c12b33c119bc8f336e49
lucasroseira <lucas.roseira@fatec.sp.gov.br>
2022-08-29 10:34:47 -0300

    fix: alteracao realizada por Lucas

---
+++ HigherBinary.cs
@@ -0,0 +1,17 @@
+﻿internal class Program {
+    private static void Main (string[] args) {
+        int[] numBinary = new int[3];
+
+        for (int i = 1; i < 4; i++) {
+            Console.WriteLine ("Type the " + i + "º binary: ");
+            numBinary[i] = Convert.ToInt32 (Console.ReadLine ());
+        }
+
+        foreach (int position in numBinary) {
+            Console.WriteLine (position);
+        }
+
+    }
+
+
+}
---
+++ VoteOrNot.cs
@@ -0,0 +1,21 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    { 
+	// Alteração realizada por Lucas
+	        string ano;
+        ano = Console.ReadLine();
+        int anoInt = Convert.ToInt16(ano);
+        if(anoInt > 15 & < 18) {
+            Console.WriteLine("Pode votar");
+        
+        } else if (anoInt >=18 ){   
+            Console.WriteLine("Tem que voltar")
+        }
+            else {
+            Console.WriteLine("Nao Pode votar");
+        }
+    
+    }
+    // Outra alteração realizada por Julio
+}
