658381172607f07d593e9202e71ce5d863f3a075
Lucas Gomes Roseira <41271185+LucasRoseira@users.noreply.github.com>
2022-08-30 23:44:26 -0300

    fix: again...

--- HigherBinary.cs
+++ HigherBinary.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿internal class Program {
     private static void Main (string[] args) {
         int[] numBinary = new int[3];
@@ -13,24 +13,4 @@
         }
 
     }
-
-
-=======
-﻿internal class Program {
-    private static void Main (string[] args) {
-        int[] numBinary = new int[3];
-
-        for (int i = 1; i < 4; i++) {
-            Console.WriteLine ("Type the " + i + "º binary: ");
-            numBinary[i] = Convert.ToInt32 (Console.ReadLine ());
-        }
-
-        foreach (int position in numBinary) {
-            Console.WriteLine (position);
-        }
-
-    }
-
-
->>>>>>> lucas
 }
